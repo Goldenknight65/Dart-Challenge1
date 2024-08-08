0e1142ec703577e8b9e72795fa9a2b847cbec63e
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-08 12:16:11 +0300

    done

--- bin/application.dart
+++ bin/application.dart
@@ -1,5 +1,54 @@
-import 'package:application/application.dart' as application;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${application.calculate()}!');
+  Map<String, List<String>> recipeManger = {};
+
+  late String userInput;
+  do {
+    print("1-add food 2- display all recipes 3- search 4- remove");
+    userInput = stdin.readLineSync()!;
+    switch (userInput.trim()) {
+      case == "1":
+        print("food name");
+        String foodName = stdin.readLineSync()!.trim();
+        print("ingredients saperate by space");
+        String recipe = stdin.readLineSync()!.trim();
+
+        List<String> recipeList = recipe.trim().split(" ");
+        recipeManger.addAll({foodName: recipeList});
+        break;
+      case == "2":
+        if (recipeManger.isNotEmpty) {
+          print(recipeManger);
+        } else {
+          print("no reicspes in the data base");
+        }
+        break;
+
+      case == "3":
+        if (recipeManger.isNotEmpty) {
+          print("name of the recipe");
+          String search = stdin.readLineSync()!;
+          if (recipeManger.containsKey(search)) {
+            print("the flowing has been remove");
+            print(recipeManger[search]);
+          } else {
+            print("no reicspes in the data base");
+          }
+        }
+        break;
+
+      case == "4":
+        if (recipeManger.isNotEmpty) {
+          print("name of the recipe");
+          String search = stdin.readLineSync()!;
+          if (recipeManger.containsKey(search)) {
+            print(recipeManger.remove(search));
+          } else {
+            print("no reicspes in the data base");
+          }
+        }
+        break;
+    }
+  } while (userInput != "0");
 }
